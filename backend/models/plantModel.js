898c059fd968c53907f022e9effb1e8574cd9221
Atae Edrissi <ataeedrissi@gmail.com>
2020-07-08 03:08:26 +0100

    add some functionality to backend

---
+++ backend/models/plantModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+//Incomplet schema
+const plantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A plant must have a name"],
+    unique: true,
+  },
+  images: [String],
+});
+
+const Plant = mongoose.model("Plant", plantSchema);
+
+module.exports = Plant;
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 🤯 Shutting down....");
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
+const app = require("./app");
+
+const uri = process.env.ATLAS_URI.replace(
+  "<password>",
+  process.env.DB_PASSWORD
+);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Successfully connected to the database!"));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}....`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHADLED REJECTION! 🤯 Shutting down....");
+  console.log(err);
+  //Here we give the server time to finish all the requests that are still panding
+  //or being handled at the time, and only after that the server is thein basically killed
+  server.close(() => {
+    process.exit(1);
+  });
+});
