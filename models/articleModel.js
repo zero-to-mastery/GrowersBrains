24d8a267b13270e391dc2ced60c208c60df89dac
Atae Edrissi <ataeedrissi@gmail.com>
2020-08-09 00:03:48 +0100

    Add static method to calcul the number of articles for each user

--- models/articleModel.js
+++ models/articleModel.js
@@ -33,7 +33,47 @@ const articleSchema = new mongoose.Schema(
     toObject: { validate: true },
   }
 );
-
+articleSchema.statics.calculNumberOfArticles = async function (authorId) {
+    const stats = await this.aggregate([
+      {
+        $match: { author: authorId },
+      },
+      {
+        $group: {
+          _id: '$author',
+          numArticle: { $sum: 1 },
+        },
+      },
+    ]);
+    // console.log(stats);
+    if(stats>0){
+    await User.findByIdAndUpdate(authorId, {
+      numberOfArticles: stats[0].numArticle,
+    });
+} else {
+    await User.findByIdAndUpdate(authorId, {
+        numberOfArticles: 0,
+    });
+  };
+  
+  //DOCUMENT MIDDLEWARE : run before .save() and .create() methods
+  articleSchema.post('save', function () {
+    this.constructor.calculNumberOfArticles(this.author);
+  });
+  
+  //This is a query middleware run before findByIdAndUpdate & findByIdAndDelete
+  //so whenever an article was updated or deleted the numberOfArticles fields also will be updated
+  //NOTE : findByIdAndUpdate is a shortand for findOneAndUpdate , same for all finById.... check mongoose documentation for more info
+  articleSchema.pre(/^findOneAnd/, async function (next) {
+    this.article = await this.findOne();
+    console.log(this.article);
+    next();
+  });
+  articleSchema.post(/^findOneAnd/, async function () {
+    //we access this.article from the pre middleware
+    await this.article.constructor.calculNumberOfArticles(this.article.author);
+  });
+  
 const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
